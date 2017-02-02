6af5e89bb5341ae0e937afbf8e5431fd9bb04f8e
Akshat Garg <coolakshat24@gmail.com>
2017-02-03 01:06:53 +0530

    Design the UI and implement the save button feature

--- popup.js
+++ popup.js
@@ -1,70 +1,157 @@
 var ans=[];
-  var b=[];
-  var a=[];
+var b=[];
+var a=[];
+var i,j, str1="", str2="", str3, k,l,m,n, str4="", str5="", str6;
+var save_btns=[];
 
-var i,j, str1="", str2="", str3;
-
-
-var x= function() {
+var x= function() 
+{
     chrome.windows.getAll(function(win_all)
     {
-    win_all.map(function(win)
-      {
-        b=[];
-        chrome.tabs.getAllInWindow(win.id, function(tab_all) 
+        win_all.map(function(win)
         {
-          a=[];
-          tab_all.map(function(tab_each) { 
-          a.push(tab_each);
-          });
-          b.push(a);
-        })
-      })
-      ans.push(b);
-      
-    })}
-
-
-
-var y= function () {
-      
-      var temp1=b.length;
-   
-      for(i=0;i<temp1;i++)
-      {
-        str1+='<div class="panel panel-default">'+
-            '<div class="panel-heading">'+
-              '<h4 class="panel-title">'+
-                '<a data-toggle="collapse" data-parent="#accordion" href="#collapse'+(i+1)+'">'+"WINDOW "+(i+1)+'</a>'+
-              '</h4>'+
-            '</div>'+
-            '<div id="collapse'+(i+1)+'" class="panel-collapse collapse in">'+
-            '<div class="panel-body">';
-            str2='';
-            var temp2=b[i].length;
-            var ans=b[i];
-            for(j=0;j<temp2;j++)
+            b=[];
+            chrome.tabs.getAllInWindow(win.id, function(tab_all) 
             {
-              tab=ans[j];
-              var f = tab.favIconUrl, t = tab.title, u = tab.url;
-              if (!u) u = '';
-              if (!t) t = u;
+                a=[];
+                tab_all.map(function(tab_each) { 
+                    a.push(tab_each);
+                });
+                b.push(a);
+            })
+        })
+        ans.push(b);
 
-               // workaround
-              if (f == 'chrome://theme/IDR_EXTENSIONS_FAVICON') f = 'data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAABF0lEQVQoz2P4z4AfMpCgQMfI6LbBZi0mnArCK73f+jyOZMWiQENOU0KHe3N01KuOg/+ZNHg0ZNSZUBSYn7T8aP3B6230y5DX9u8tP5p+suZDURD6IOJV1Mu4FynPE19Ev4x85fvmPweKgrj76c8ykWDkKwtOTVUNHriC5atfLNy2Nf05TEHMS9d9Fh9M98IVvLb5z7c5IOEFTEHy88yrIa+DH8AVmDwy7/BeEwdXkPrsf+3aHXsXwhX4P/V7E/I6+8HpNUCLgAqiX7qw/rf+rwZXkHEj8UXii5aj/1Xv26c+yXwW8SpIAsUX1fvz72c/ir38n8EtO/Fx1Y2ai63yqEEd97/zf///eiDL8X/f/9r/Wf95iI5NALw5DuHmTOHfAAAAAElFTkSuQmCC';
-              str3= '<div class="list-group-item" id="item">'+'<img src='+f+' /> <div id="text">'+t+'</div></div>';
-              str2+=str3;
-            }
-        str1+=str2+
-       '</div>'+
-       '</div>';
-      }
-      document.getElementById('accordion').innerHTML=str1;
-    };
+    }
+)}
 
+
+
+var y= function () 
+{
+    var temp1=b.length;
+
+    for(i=0;i<temp1;i++)
+    {
+        str1+='<div class="panel panel-default" id="window_'+(i+1)+'" >'+
+        '<div class="panel-heading">'+
+        '<h4 class="panel-title">'+
+        '<a data-toggle="collapse" data-parent="#current" href="#collapse'+(i+1)+'">'+"WINDOW "+(i+1)+'</a>'+
+        '<button type="button" id='+(i+1)+' class="btn btn-default btn-xs save_btn">'+ "Save" +'</button>'+
+        '</h4>'+
+        '</div>'+
+        '<div id="collapse'+(i+1)+'" class="panel-collapse collapse in">'+
+        '<div class="panel-body">';
+        str2='';
+        var temp2=b[i].length;
+        var ans=b[i];
+        for(j=0;j<temp2;j++)
+        {
+            tab=ans[j];
+            var f = tab.favIconUrl, t = tab.title, u = tab.url;
+            if (!u) u = '';
+            if (!t) t = u;
+
+            // workaround
+            if (f == 'chrome://theme/IDR_EXTENSIONS_FAVICON') f = 'data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAABF0lEQVQoz2P4z4AfMpCgQMfI6LbBZi0mnArCK73f+jyOZMWiQENOU0KHe3N01KuOg/+ZNHg0ZNSZUBSYn7T8aP3B6230y5DX9u8tP5p+suZDURD6IOJV1Mu4FynPE19Ev4x85fvmPweKgrj76c8ykWDkKwtOTVUNHriC5atfLNy2Nf05TEHMS9d9Fh9M98IVvLb5z7c5IOEFTEHy88yrIa+DH8AVmDwy7/BeEwdXkPrsf+3aHXsXwhX4P/V7E/I6+8HpNUCLgAqiX7qw/rf+rwZXkHEj8UXii5aj/1Xv26c+yXwW8SpIAsUX1fvz72c/ir38n8EtO/Fx1Y2ai63yqEEd97/zf///eiDL8X/f/9r/Wf95iI5NALw5DuHmTOHfAAAAAElFTkSuQmCC';
+            str3= '<div class="list-group-item" id="item">'+'<img src='+f+' /> <div id="text">'+t+'</div></div>';
+            str2+=str3;
+        }
+        str1+=str2+'</div>'+'</div>'+'</div>';
+    }
+    document.getElementById('current').innerHTML=str1;
+};
+
+
+function save_files(clicked_btn)
+{
+    var clicked_id=parseInt(clicked_btn.toElement.id);
+    
+    var temp=[];
+    var this_win=b[clicked_id-1];
+    
+    var len=this_win.length;
+    
+    for(k=0;k<len;k++)
+    {
+        temp.push(this_win[k]);
+    }
+    var abc=JSON.stringify(temp);
+    var flag=1;
+    for(var i=0;i<localStorage.length;i++)
+    {
+        if(localStorage.getItem("saved_window"+(i+1))==abc)
+        {
+            alert('Already saved! Checkout saved tabs!');
+            flag=0;
+            break;
+        }
+
+    }
+    if(flag==1)
+    {
+        localStorage.setItem("saved_window"+(localStorage.length+1), JSON.stringify(temp));
+        z();
+    }
+}
+
+var z= function () 
+{
+    var temp1=localStorage.length;
+    var saved_windows=[];
+    for(l=0;l<temp1;l++)
+    {
+        saved_windows.push( JSON.parse( localStorage.getItem("saved_window"+(l+1)) ));
+    }
+    str4='';
+    for(l=0;l<temp1;l++)
+    {
+        str4+='<div class="panel panel-default">'+
+        '<div class="panel-heading">'+
+        '<h4 class="panel-title">'+
+        '<a data-toggle="collapse" data-parent="#saved" href="#collapse'+(l+1)+'">'+"WINDOW "+(l+1)+'</a>'+
+        '<button type="button" id='+(l+1)+' class="btn btn-default btn-xs"> Load </button>'+
+        '</h4>'+
+        '</div>'+
+        '<div id="collapse'+(l+1)+'" class="panel-collapse collapse in">'+
+        '<div class="panel-body">';
+        str5='';
+        var temp2=saved_windows[l].length;
+        var ans=saved_windows[l];
+        for(m=0;m<temp2;m++)
+        {
+            tab=ans[m];
+            var f = tab.favIconUrl, t = tab.title, u = tab.url;
+            if (!u) u = '';
+            if (!t) t = u;
+
+            // workaround
+            if (f == 'chrome://theme/IDR_EXTENSIONS_FAVICON') f = 'data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAABF0lEQVQoz2P4z4AfMpCgQMfI6LbBZi0mnArCK73f+jyOZMWiQENOU0KHe3N01KuOg/+ZNHg0ZNSZUBSYn7T8aP3B6230y5DX9u8tP5p+suZDURD6IOJV1Mu4FynPE19Ev4x85fvmPweKgrj76c8ykWDkKwtOTVUNHriC5atfLNy2Nf05TEHMS9d9Fh9M98IVvLb5z7c5IOEFTEHy88yrIa+DH8AVmDwy7/BeEwdXkPrsf+3aHXsXwhX4P/V7E/I6+8HpNUCLgAqiX7qw/rf+rwZXkHEj8UXii5aj/1Xv26c+yXwW8SpIAsUX1fvz72c/ir38n8EtO/Fx1Y2ai63yqEEd97/zf///eiDL8X/f/9r/Wf95iI5NALw5DuHmTOHfAAAAAElFTkSuQmCC';
+            str6 = '<div class="list-group-item" id="item">'+'<img src='+f+' /> <div id="text">'+t+'</div></div>';
+            str5+=str6;
+        }
+        str4+=str5+'</div>'+'</div>'+'</div>';
+    }
+
+    document.getElementById('saved').innerHTML=str4;
+};
+
+
+//Function calls
 x();
 
 setTimeout(function() {
-  y();
-}, 500);
+    y();    
+    z();
 
+    save_btns = $('.save_btn');
+    var mouse_event;
+    for (i = 0; i < save_btns.length; i++) 
+        {
+            
+            save_btns[i].addEventListener("click",  function(mouse_event) {    
+                
+                save_files(mouse_event);
+            });
+       }
+}, 500);
