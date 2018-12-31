d6489215dd9e34eb2566496b7b7f93f007c7c2f4
rkishan516 <34465683+rkishan516@users.noreply.github.com>
2018-12-31 08:42:51 +0530

    Added New Design For the Extension (#15)
    * New Design For extension
    
    * Updated Title Position
    
    * Theme Change
    
    * Updated for Solid Colors
    
    * Added space for Theme Switcher

--- popup.js
+++ popup.js
@@ -28,6 +28,7 @@ var current_tab_data = () => {
     var no_of_windows=single_window.length;
 
     for(i=0;i<no_of_windows;i++) {
+        var second_tab = 0;
         str1+='<div class="panel panel-default" id="window_'+(i+1)+'" >'+
         '<div class="panel-heading">'+
         '<h4 class="panel-title">'+
@@ -36,7 +37,7 @@ var current_tab_data = () => {
         '</h4>'+
         '</div>'+
         '<div id="collapse'+(i+1)+'" class="panel-collapse collapse in">'+
-        '<div class="panel-body">';
+        '<div class="panel-body"><table>';
         str2='';
         var no_of_tabs=single_window[i].length;
         var tabs_in_window=single_window[i];
@@ -45,13 +46,26 @@ var current_tab_data = () => {
             var f = tab.favIconUrl, t = tab.title, u = tab.url;
             if (!u) u = '';
             if (!t) t = u;
+            if(t.length > 17){
+                t = t.substring(0,18);
+                t += '...';
+            }
 
             // workaround
             if (f == 'chrome://theme/IDR_EXTENSIONS_FAVICON') f = 'data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAABF0lEQVQoz2P4z4AfMpCgQMfI6LbBZi0mnArCK73f+jyOZMWiQENOU0KHe3N01KuOg/+ZNHg0ZNSZUBSYn7T8aP3B6230y5DX9u8tP5p+suZDURD6IOJV1Mu4FynPE19Ev4x85fvmPweKgrj76c8ykWDkKwtOTVUNHriC5atfLNy2Nf05TEHMS9d9Fh9M98IVvLb5z7c5IOEFTEHy88yrIa+DH8AVmDwy7/BeEwdXkPrsf+3aHXsXwhX4P/V7E/I6+8HpNUCLgAqiX7qw/rf+rwZXkHEj8UXii5aj/1Xv26c+yXwW8SpIAsUX1fvz72c/ir38n8EtO/Fx1Y2ai63yqEEd97/zf///eiDL8X/f/9r/Wf95iI5NALw5DuHmTOHfAAAAAElFTkSuQmCC';
-            str3= '<div class="list-group-item" id="item">'+'<img src='+f+' /> <div id="text">'+t+'</div></div>';
+            if(second_tab==3){
+                str3= '<td><div class="list-group-item" id="item">'+'<div class="circle"><img src='+f+' /></div> <div id="text"><center>'+t+'</center></div></div></td></tr>';
+                second_tab= 0;
+            }else if(second_tab == 0){
+                str3 = '<tr><td><div class="list-group-item" id="item">'+'<div class="circle"><img src='+f+' /></div> <div id="text"><center>'+t+'</center></div></div></td>';
+                second_tab++;
+            }else{
+                str3 = '<td><div class="list-group-item" id="item">'+'<div class="circle"><img src='+f+' /></div> <div id="text"><center>'+t+'</center></div></div></td>';
+                second_tab++;
+            }
             str2+=str3;
         }
-        str1+=str2+'</div>'+'</div>'+'</div>';
+        str1+=str2+'</table>'+'</div>'+'</div>'+'</div>';
     }
     document.getElementById('current').innerHTML=str1;
 };
@@ -64,6 +78,7 @@ var saved_tab_data =  () => {
     }
     str4='';
     for(l=0;l<total_windows;l++) {
+        var second_tab = 0;
         str4+='<div class="panel panel-default">'+
         '<div class="panel-heading">'+
         '<h4 class="panel-title">'+
@@ -73,7 +88,7 @@ var saved_tab_data =  () => {
         '</h4>'+
         '</div>'+
         '<div id="collapse'+(l+1)+'" class="panel-collapse collapse in">'+
-        '<div class="panel-body">';
+        '<div class="panel-body"><table>';
         str5='';
         var no_of_tabs=saved_windows[l].length;
         var tabs_in_window=saved_windows[l];
@@ -82,13 +97,26 @@ var saved_tab_data =  () => {
             var f = tab.favIconUrl, t = tab.title, u = tab.url;
             if (!u) u = '';
             if (!t) t = u;
+            if(t.length > 17){
+                t = t.substring(0,18);
+                t += '...';
+            }
 
             // workaround
             if (f == 'chrome://theme/IDR_EXTENSIONS_FAVICON') f = 'data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAABF0lEQVQoz2P4z4AfMpCgQMfI6LbBZi0mnArCK73f+jyOZMWiQENOU0KHe3N01KuOg/+ZNHg0ZNSZUBSYn7T8aP3B6230y5DX9u8tP5p+suZDURD6IOJV1Mu4FynPE19Ev4x85fvmPweKgrj76c8ykWDkKwtOTVUNHriC5atfLNy2Nf05TEHMS9d9Fh9M98IVvLb5z7c5IOEFTEHy88yrIa+DH8AVmDwy7/BeEwdXkPrsf+3aHXsXwhX4P/V7E/I6+8HpNUCLgAqiX7qw/rf+rwZXkHEj8UXii5aj/1Xv26c+yXwW8SpIAsUX1fvz72c/ir38n8EtO/Fx1Y2ai63yqEEd97/zf///eiDL8X/f/9r/Wf95iI5NALw5DuHmTOHfAAAAAElFTkSuQmCC';
-            str6 = '<div class="list-group-item" id="item">'+'<img src='+f+' /> <div id="text">'+t+'</div></div>';
+            if(second_tab==3){
+                str6= '<td><div class="list-group-item" id="item">'+'<div class="circle"><img src='+f+' /></div> <div id="text"><center>'+t+'</center></div></div></td></tr>';
+                second_tab= 0;
+            }else if(second_tab == 0){
+                str6 = '<tr><td><div class="list-group-item" id="item">'+'<div class="circle"><img src='+f+' /></div> <div id="text"><center>'+t+'</center></div></div></td>';
+                second_tab++;
+            }else{
+                str6 = '<td><div class="list-group-item" id="item">'+'<div class="circle"><img src='+f+' /></div> <div id="text"><center>'+t+'</center></div></div></td>';
+                second_tab++;
+            }
             str5+=str6;
         }
-        str4+=str5+'</div>'+'</div>'+'</div>';
+        str4+=str5+'</table>'+'</div>'+'</div>'+'</div>';
     }
     document.getElementById('saved').innerHTML=str4;
 };
